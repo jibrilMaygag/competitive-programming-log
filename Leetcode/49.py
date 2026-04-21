113d3a7f6a28964f14bfc59c2eb7d568a6755107
Jibril <jibrilmeygag@gmail.com>
2026-04-21 21:33:43 +0300

    solved leetcode 49: Group Anagrams

---
+++ Leetcode/49.py
@@ -0,0 +1,17 @@
+def sortC(s):
+    arr=[0]*26
+    for char in s:
+        arr[ord(char)-ord('a')]+=1
+
+    return "".join([f"{chr(i+ord('a'))}{arr[i]}" for i in range(len(arr)) if arr[i]!=0])
+
+def groupAnagrams( strs):
+    res={}
+    for str in strs:
+        sorted=sortC(str) #"10001000000000000001000000" str="ate"
+        if sorted in res:
+            res[sorted].append(str)
+        else:
+            res[sorted]=[str]
+    return [value for _,value in res.items()]
+        
